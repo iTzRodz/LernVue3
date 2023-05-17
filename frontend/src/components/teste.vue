8f9dfdae08847f8681b5d4acb4d5340c8413cd96
Rodolfo <rndcsjj@gmail.com>
2023-05-17 13:23:32 -0300

    contador

---
+++ frontend/src/components/teste.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="app">
+    <button @click="increment">Clique aqui para adicionar 1</button>
+    <p>O contador está em {{ count }}</p>
+  </div>
+</template>
+<script>
+export default {
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increment() {
+        this.count++
+      }
+    }
+  }
+  
