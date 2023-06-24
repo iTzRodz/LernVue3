13b6447dd119ed15294b44c782f4cc7910a700fd
Rodolfo Win <rndcsjj@gmail.com>
2023-06-24 08:23:18 -0300

    cria funcionalidade para listar os dados na tabela

--- frontend/src/components/teste.vue
+++ frontend/src/components/teste.vue
@@ -1,65 +1,35 @@
 <template>
-  <div id="app">
-    <p>O contador está em {{ count }}</p>
-    <button v-on:click="count++">Adicionar</button>
-
-    {{ userName }}
-  </div>
-  
-  <div>
-    <img :src="image" alt="" srcset="">
-  </div>
-
-  <div>
-    <ul>
-      <template v-for="(user, key) in users" :key="index">
-      <li v-if="user.is_admin === 1">
-          {{ user.firstName }} | {{ key }}
-        </li>
-      </template>
-    </ul>
-
-  </div>
+  <h1>Lista de users</h1>
+  <ul>
+    <li v-for="user in users.user" :key="user.id"> {{ user }}</li>
+  </ul>
+  {{ users }}
 </template>
 
 <script setup>
 import { onMounted, onUpdated, reactive, ref } from 'vue';
+import http from '@/services/http.js';
 
-const image = 'https://picsum.photos/200/300';
+const users = reactive({user: []})
 
-const user = reactive({ firstName: "teste", age: 30 })
 
-const users = reactive([
-  {
-    id: 1,
-    firstName: 'Rodolfo',
-    age: 50,
-    is_admin: 1
-  },
-  {
-    id: 2,
-    firstName: 'Rodolfo',
-    age: 89,
-    is_admin: 0
-  },
-  {
-    id: 3,
-    firstName: 'Rodolfo cAR',
-    age: 89,
-    is_admin: 1
-  },
-])
 
-const userName = ref('Rodolfo')
-const count = ref(0);
-var nome = `rodolfo`
-onMounted(() => {
-  console.log('Mounted');
-  console.log(nome);
+const listUsers = async () => {
+  try {
+    const { data } = await http.get('api/users')
+    users.user = data
+    console.log(data);
+    console.log(users);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+onMounted( async() => {
+  await listUsers();
+
 })
 
-onUpdated(() => {
-  console.log('updated');
-})
+
 
 </script>  
